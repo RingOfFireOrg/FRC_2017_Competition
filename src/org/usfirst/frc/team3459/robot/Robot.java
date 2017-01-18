d1f59ec3abee22f6cdec2551081a1a32530f9a37
5_Boron <5_Boron@WakeRobotics.org>
2017-01-17 21:58:14 -0500

    changed from sample robot to iterative robot

--- src/org/usfirst/frc/team3459/robot/Robot.java
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -1,55 +1,70 @@
 package org.usfirst.frc.team3459.robot;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
-import com.ctre.CANTalon; 
+import edu.wpi.first.wpilibj.livewindow.LiveWindow; 
 
 /**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
+ * Don't change the name of this or it won't work. (The manifest looks for "Robot")
  */
-public class Robot extends SampleRobot {
-	HDrive myRobot = new HDrive(RobotMap.leftMotor, RobotMap.rightMotor, RobotMap.shimmyMotor); // class that handles basic drive
-												// operations
-		
+public class Robot extends IterativeRobot {
+	
+	/*
+	 * Member variables go here 
+	 */
+	HDrive driveTrain = new HDrive(RobotMap.leftMotor, RobotMap.rightMotor, RobotMap.shimmyMotor); // class that handles basic drive
+	// operations
+
 	Joystick leftStick = new Joystick(RobotMap.leftStick);
 	Joystick rightStick = new Joystick(RobotMap.rightStick);
 	
-	public Robot() {
-		myRobot.setExpiration(0.1);
-	}
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {        
 
-	/**
-	 * Runs the motors with tank steering.
-	 */
-	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(leftStick, rightStick);
-			Timer.delay(0.005); // wait for a motor update time
-			if(leftStick.getTrigger()) {
-				myRobot.shimmyLeft();
-			}
-			else if(rightStick.getTrigger()){
-				myRobot.shimmyRight();
-			}
-			else{
-				myRobot.shimmyStop();
-			}
+    }
+      
+    /**
+     * This function is called once when we go into the teleop mode
+     */
+    public void teleopInit(){
+    }
+    
+    /**
+     * This function is called periodically during operator control (approx 20ms)
+     */
+    public void teleopPeriodic() {
+		driveTrain.tankDrive(leftStick, rightStick);
+		if(leftStick.getTrigger()) {
+			driveTrain.shimmyLeft();
 		}
+		else if(rightStick.getTrigger()){
+			driveTrain.shimmyRight();
+		}
+		else{
+			driveTrain.shimmyStop();
+		}
+    }
 
-		}
-	}
+    /**
+     * This function is called once when we go into the Autonomous mode
+     */
+    public void autonomousInit(){
+    }
+
+    /**
+     * This function is called periodically during autonomous control (approx 20ms)
+     */
+    public void autonomousPeriodic() {
+     }
+
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run(); // This makes sure the values of items are correct on the driver station during test mode.   
+    }    
+}
 
