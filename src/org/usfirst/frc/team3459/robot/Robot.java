1792fe4918b7d6fa41ac6abddb876ef8219a9b8b
Alan Smith <alan@randomsmiths.com>
2017-02-06 14:51:38 -0500

    Cleanout unneeded items
    Remove TalonSRX from userlib
    Remove unneeded CameraServer from robot.java

--- src/org/usfirst/frc/team3459/robot/Robot.java
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team3459.robot;
 
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
