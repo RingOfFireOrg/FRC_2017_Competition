1f4346c13a47278de654d3749386562e0587f627
Alan Smith <alan@randomsmiths.com>
2017-02-06 14:53:40 -0500

    Cleanup
    Remove unneeded imports from PTDrive.java

--- src/org/usfirst/frc/team3459/robot/PTDrive.java
+++ src/org/usfirst/frc/team3459/robot/PTDrive.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team3459.robot;
 
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PTDrive extends RobotDrive {
